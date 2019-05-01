72ff8a006ab4409f5f6b0cb7468abedc7d5d784d
wrogdenmoore <46682569+wrogdenmoore@users.noreply.github.com>
2019-04-30 23:46:34 -0400

    Update SportsBetting.py
    Printing the recommendation

--- SportsBetting.py
+++ SportsBetting.py
@@ -67,3 +67,18 @@ first_rating=db[db['TEAM']==first_team]['Rating'] #storing the team rating (base
 
 second_rating=db[db['TEAM']==second_team]['Rating'] #storing the second team rating
 
+print()
+print('The model uses these correlations to weight each statistic and output a recommendation')#explaining the use of the weight for the model
+# print(second_rating[int(usr_choice2)])
+input('Press enter to continue to the model recomendation:') #breaking one more team before the recommendation
+print()
+print()
+print()
+
+if first_rating[int(usr_choice1)] > second_rating[int(usr_choice2)]: #if the first rating is bigger than the second team's rating
+    print("We recommend choosing the ",first_team, "based on our model.") #print the recommendation for the first team's name
+else:
+    print("We recommend choosing the ",second_team, "based on our model.") #print the recommendation for the second team's name
+
+print()
+print()
