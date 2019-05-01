2985a9a5b8a7381bb89cd3d9c730ad066a36bf87
wrogdenmoore <46682569+wrogdenmoore@users.noreply.github.com>
2019-04-30 23:28:35 -0400

    Update SportsBetting.py

--- SportsBetting.py
+++ SportsBetting.py
@@ -1,5 +1,8 @@
 import pandas as pd 
 #pandas to utilize excel
 
-dbshow=pd.read_excel('database1.xlsx', 'PresentData')
-print(dbshow)
+dbshow=pd.read_excel('database1.xlsx', 'PresentData') #bring in excel file
+
+print(dbshow) #print the data from the present data page (print the main statistics for each team)
+
+
