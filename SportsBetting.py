abfde6e23c793c9fd6ead91fa998f37da7123a91
wrogdenmoore <46682569+wrogdenmoore@users.noreply.github.com>
2019-04-30 23:37:20 -0400

    Update SportsBetting.py
    Allows for the user to input two numbers corresponding to each team selection

--- SportsBetting.py
+++ SportsBetting.py
@@ -12,21 +12,21 @@ print('See above for all the NBA teams and their statistics.') #view the statist
 print()
 input('Press enter to continue on to team selection') #this is to break up the steps in the program and introduce the next step
 
-db_inputteam=db['TEAM'] 
-#just concerned with the team column
-# print()
-# print('Please choose a team from this list', '\n' , db_inputteam)
-# print()
+db_inputteam=db['TEAM']  #first team
+just concerned with the team column
+print()
+print('Please choose a team from this list', '\n' , db_inputteam) #list of nba teams with a number next to them
+print()
 
-# usr_choice1=input('Enter the number next to your team of choice above: ')
+usr_choice1=input('Enter the number next to your team of choice above: ') #input user choice
 
-# first_team=db_inputteam[int(usr_choice1)]
-# print()
-# #2nd team 
+first_team=db_inputteam[int(usr_choice1)] #integer next to team of choice
+print()
+#2nd team 
 
 
-# db_inputsecond=db['TEAM']
-# print()
-# print('Please choose a different team from this list', '\n' , db_inputsecond)
-# print()
-# usr_choice2=input('Enter the number next to your team of choice above: ')
+db_inputsecond=db['TEAM'] #second team
+print()
+print('Please choose a different team from this list', '\n' , db_inputsecond) #choose a different team from the list
+print()
+usr_choice2=input('Enter the number next to your team of choice above: ') #put in the number
